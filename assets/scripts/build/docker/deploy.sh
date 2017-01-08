19dbe77b99c1eed050ceb740dd92bfe36457d295
admin <admin@cryptaz.org>
2017-01-08 20:09:34 +0700

    Corrected crlf

--- assets/scripts/build/docker/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Importing keys and setting up repositories"
-echo "deb https://packagecloud.io/grafana/stable/debian/ jessie main" > /etc/apt/sources.list.d/grafana.list
-echo "deb https://repos.influxdata.com/debian jessie stable" > /etc/apt/sources.list.d/influxdb.list
-curl https://packagecloud.io/gpg.key | sudo apt-key add -
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-apt-get update
-echo "Installing dependencies"
-apt-get -y install default-jdk maven sudo apt-transport-https curl grafana influxdb git wget nano sudo curl
